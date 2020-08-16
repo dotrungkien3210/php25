56c506f3bb694991214fda7533667c5eae1ae5c5
dotrungkien3210 <dotrungkien10c@gmail.com>
2020-08-17 03:20:16 +0700

    homework2

---
+++ homework2/2.7/posts.php
@@ -0,0 +1,11 @@
+<?php
+include_once('header.php');
+ ?>
+
+ <div class="container-fluid">
+          <h1 class="h3 mb-4 text-gray-800">Các bài viết</h1>
+
+        </div>
+<?php
+include_once('footer.php');
+ ?>
---
+++ homework2/2.7/users.php
@@ -0,0 +1,12 @@
+<?php
+include_once('header.php');
+ ?>
+
+ <div class="container-fluid">
+          <h1 class="h3 mb-4 text-gray-800">Người dùng</h1>
+
+        </div>
+    </div>
+<?php
+include_once('footer.php');
+ ?>
